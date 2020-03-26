0f83b20ed4e53004750de8f9bfaf3de8db893f43
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2020-03-26 01:47:20 -0600

    Add validotrs for inclusion, gt gte lt lte

--- test/validators_test.exs
+++ test/validators_test.exs
@@ -37,4 +37,49 @@ defmodule ValidatorsTest do
     results = Skooma.valid?(test_data, test_schema)
     assert(expected_results == results)
   end
+
+  test "inclusion function" do
+    test_data = 15
+    test_schema = [:list, Validators.inclusion([1..5])]
+    expected_results = {:error, ["Value is not included in the options: [1..5]"]}
+
+    results = Skooma.valid?(test_data, test_schema)
+    assert(expected_results == results)
+  end
+
+  test "gt function" do
+    test_data = 6
+    test_schema = [:int, Validators.gt(6)]
+    expected_results = {:error, ["Value has to be greater than 6"]}
+
+    results = Skooma.valid?(test_data, test_schema)
+    assert(expected_results == results)
+  end
+
+  test "gte function" do
+    test_data = 5
+    test_schema = [:int, Validators.gte(6)]
+    expected_results = {:error, ["Value has to be greater or equal than 6"]}
+
+    results = Skooma.valid?(test_data, test_schema)
+    assert(expected_results == results)
+  end
+
+  test "lt function" do
+    test_data = 4
+    test_schema = [:int, Validators.lt(4)]
+    expected_results = {:error, ["Value has to be less than 4"]}
+
+    results = Skooma.valid?(test_data, test_schema)
+    assert(expected_results == results)
+  end
+
+  test "lte function" do
+    test_data = 5
+    test_schema = [:int, Validators.lte(4)]
+    expected_results = {:error, ["Value has to be less or equal than 4"]}
+
+    results = Skooma.valid?(test_data, test_schema)
+    assert(expected_results == results)
+  end
 end
